b443537f51901a6e008f233e87767df25402e8e5
Daniel <dmonar2@uic.edu>
2023-02-16 23:56:37 -0600

    New branch

--- src/main/java/dev/munaritz/Movies/MoviesApplication.java
+++ src/main/java/dev/munaritz/Movies/MoviesApplication.java
@@ -2,8 +2,10 @@ package dev.munaritz.Movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
+//@Controller("/")
 public class MoviesApplication {
 
 	public static void main(String[] args) {
